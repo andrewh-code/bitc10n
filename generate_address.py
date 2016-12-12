0efdac0ec20b85db83eeeeead092ce062f7f654a
andrewh-code <andrewh.code@gmail.com>
2016-12-12 11:23:25 -0500

    add file for address generation/storing
    add file for address generation/storing

---
+++ generate_address.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+# class generate addresses
+
+# use ECC to generate the private/pub key
+
+# generate wallet address from pub key
+    # algorithm to extract address from pub key (prob just use pub key)
+
+
+# store wallet address in hash table with associated name
+
+# put in keystore 
+
+
+
