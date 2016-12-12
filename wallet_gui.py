69f46285f42c293f6b121890893b13fc658020e6
andrewh-code <andrewh.code@gmail.com>
2016-12-12 17:17:28 -0500

    wallet gui
    wallet gui

---
+++ wallet_gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python 
+
+
+# include libraries
+import os
+import sys
+import pyelliptic
+import tkinter as tk     # lowercase t for python 3.x
+
+
+window = tk.Tk()
+
+
+
+
+
+
+window.mainloop()
